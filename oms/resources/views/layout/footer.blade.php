78e36986fadd143db968279eae170265caee6455
Farzad Rahman <farzadrahman59@gmail.com>
2018-04-12 15:12:02 +0600

    Farzad

--- oms/resources/views/layout/footer.blade.php
+++ oms/resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    2018 &copy; Teknovisual . | Designed & Developed by <a target="_blank" href="#">Teknovisual.</a>
+    2018 &copy; Tekno Visual . | Designed & Developed by <a target="_blank" href="#">Tekno Visual.</a>
     <a href="#" class="go-top">
         <i class="fa fa-angle-up"></i>
     </a>
